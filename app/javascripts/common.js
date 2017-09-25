656a98d1289c7ca2e6c6dc1f9e3950bdefbead39
Alex Gashkov <gashkov321@gmail.com>
2017-09-25 19:57:39 +0300

    desktop version

--- app/javascripts/common.js
+++ app/javascripts/common.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
+	//anchor
+    $("a.ancLinks").click(function () { 
+      var elementClick = $(this).attr("href");
+      var destination = $(elementClick).offset().top;
+      $('html,body').animate( { scrollTop: destination }, 2000 );
+      return false;
+    });
 
-});
+
+    
+  });
